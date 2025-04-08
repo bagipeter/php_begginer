f257422aa4978d45548c20abab2065dd14116e93
Bagi Péter <bgipeter877@gmail.com>
2025-04-08 14:55:29 +0200

    steps

--- str.php
+++ str.php
@@ -3,6 +3,7 @@
 $str = "Sziasztok ez egy php str";
 echo strtolower($str). "<br>";
 echo strtoupper($str). "<br>";
+echo strtoupper($str). "<br>";
 echo str_replace("php", "Valami más", $str). "<br>";
 echo  str_word_count($str)."<br>";
 
