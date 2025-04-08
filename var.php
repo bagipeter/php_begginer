2fcf1930fe5d5c0554599a5c32d03922c0345d52
Bagi Péter <bgipeter877@gmail.com>
2025-04-08 14:54:19 +0200

    first steps

--- var.php
+++ var.php
@@ -20,10 +20,31 @@ echo $salary . '<br>'; */
 
 //print variables type | változotipusok kiiratása
 
-echo gettype($name). "<br>";
+/* echo gettype($name). "<br>";
 echo gettype($age). "<br>";
 echo gettype($isFather). "<br>";
 echo gettype($money). "<br>";
-echo gettype($salary). "<br>";
+echo gettype($salary). "<br>"; */
+
+//change value and print | érték megvátoztatása és kiiratás
+
+/* $name = 23;
+echo gettype($name);
+echo "<br>" */
+
+/* var_dump(is_string($name));
+echo "<br>";
+
+var_dump(is_string($age));
+echo "<br>";
+
+var_dump(is_int($age));
+echo "<br>"; */
+
+//check if variable is defined | Ellenőrizze, hogy a változó definiálva van-e
+
+/* var_dump(isset($name));
+echo "<br>";
+var_dump(isset($city)); */
 
 ?>
